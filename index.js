8f269f7f9f295656d3f31ff434749b5ea7bfaf80
Maksym <strangerssoftware@gmail.com>
2023-06-27 19:25:14 +0300

    Update project to read settings from file

--- index.js
+++ index.js
@@ -3,52 +3,55 @@ const bodyParser = require('body-parser');
 const network = require('./utils/network');
 const hardware = require('./utils/hardware');
 const snmp = require("net-snmp");
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+
 const port = 3377;
 
-const controlledHostIP = '192.168.43.107';
+const controlledHostIP = '127.0.0.1';
 
-
-let globalProps = {
-  authKey: "snmpv3pass",
-  community: 'public',
-  cpu: 60,    
-  disk: 90,    
-  host: '',
-  name: "snmpv3user",
-  privKey: "cryptov3pass",
-  ram: 80,  
-  security: '0',
-  trackLan: false,
-  trapReceiverIP: '127.0.0.1',
+var global_params = {
+  community_name: '',
+  cpu_threshold: 0,
+  disk_threshold: 0,
+  inform_network_status: true,
+  port_number: 162,
+  ram_threshold: 0,
+  run_every_in_minute: 5,
+  security: 'NoAuthNoPriv',
+  services_list: '',
+  trap_receiver_ip: '',
+  user_key: '',
+  user_name: '',
+  user_password: '',
 };
 
 function getUser() {
 
-  const level = globalProps.security === '0'
+  const level = global_params.security === 'NoAuthNoPriv'
     ? snmp.SecurityLevel.noAuthNoPriv
-    : globalProps.security === '1'
+    : global_params.security === 'AuthNoPriv'
       ? snmp.SecurityLevel.authNoPriv
       : snmp.SecurityLevel.authPriv
 
   return {
-    authKey: globalProps.authKey,
+    authKey: global_params.user_password,
     authProtocol: snmp.AuthProtocols.md5,
     level,
-    name: globalProps.name,
-    privKey: globalProps.privKey,
+    name: global_params.user_name,
+    privKey: global_params.user_key,
     privProtocol: snmp.PrivProtocols.aes,
   }
 }
 
 app.get('/', async (req, res) => {
   let host;
-  const session = globalProps.security === '0'
-    ? snmp.createSession(controlledHostIP, globalProps.community)
+  const session = global_params.security === 'NoAuthNoPriv'
+    ? snmp.createSession(controlledHostIP, global_params.community_name)
     : snmp.createV3Session (controlledHostIP, getUser())
 
   session.get(["1.3.6.1.2.1.1.5.0"], function (error, oid_data) {
@@ -63,18 +66,18 @@ app.get('/', async (req, res) => {
       }
     }
 
-    res.render('pages/index', {...globalProps, host });
+    res.render('pages/index', {...global_params, host });
     session.close();
   });
 });
 
 app.post('/', (req, res) => {
   let host = 'Connection error!';
-  globalProps = {...globalProps, ...req.body};
-  globalProps.trackLan = req.body?.trackLan ? true : false;
+  global_params = {...global_params, ...req.body};
+  global_params.inform_network_status = req.body.inform_network_status ? true : false;
 
-  const session = globalProps.security === '0'
-    ? snmp.createSession(controlledHostIP, globalProps.community)
+  const session = global_params.security === 'NoAuthNoPriv'
+    ? snmp.createSession(controlledHostIP, global_params.community_name)
     : snmp.createV3Session (controlledHostIP, getUser())
 
   session.get(["1.3.6.1.2.1.1.5.0"], function (error, oid_data) {
@@ -89,7 +92,7 @@ app.post('/', (req, res) => {
       }
     }
 
-    res.render('pages/index', {...globalProps, host });
+    res.render('pages/index', {...global_params, host });
     session.close();
   });
 
@@ -98,40 +101,84 @@ app.post('/', (req, res) => {
 
 
 var global_net_adapters = [];
+var checkInterval = null;
+
+const paramsToCheck = [
+  'community_name', 
+  'cpu_threshold', 
+  'disk_threshold', 
+  'inform_network_status', 
+  'port_number',
+  'ram_threshold', 
+  'run_every_in_minute',
+  'security',
+  'services_list',
+  'trap_receiver_ip',
+  'user_key',
+  'user_name',
+  'user_password',
+]
 
 setInterval(() => {
+  let rawData = fs.readFileSync('/etc/local/etc/global_config.json');
+  let allConfig = JSON.parse(rawData);
+  console.log('duty config file check');
 
-  const session = globalProps.security === '0'
-    ? snmp.createSession(controlledHostIP, globalProps.community)
-    : snmp.createV3Session (controlledHostIP, getUser())
+  if(allConfig.snmp_config && allConfig.snmp_config.length) {
+    
+    const snmp_config= allConfig.snmp_config.find(({is_default}) => is_default)
 
-  const trapReceiver = snmp.createSession(globalProps.trapReceiverIP, globalProps.community);
-
-  if (globalProps.trackLan) {
-    var network_oid = "1.3.6.1.2.1.2.2";
-    var lan_adapter_columns = [2, 7, 8, 6];
-    const checkAdapters = (error, table) => {
-      global_net_adapters = [...network.agentCheck(error, table, trapReceiver, global_net_adapters)];
+    if ((paramsToCheck.some((param) => global_params[param] != snmp_config[param])) && checkInterval) {
+      console.log('previous config cancelled');
+      clearInterval(checkInterval);
+      checkInterval = null;
     }
-    session.tableColumns(network_oid, lan_adapter_columns, 1, checkAdapters);
-  }
 
-  var disks_oid = "1.3.6.1.2.1.25.2.3";
-  var disks_columns = [2, 3, 4, 5, 6];
+    if (!checkInterval) {
+      console.log('config selected and started -', snmp_config);
 
-  const checkDisks = (error, table) => hardware.agentCheckDisks(error, table, trapReceiver, globalProps.disk);
-  session.tableColumns(disks_oid, disks_columns, 20, checkDisks);
+      global_params = snmp_config;
 
-  const checkRam = (error, table) => hardware.agentCheckRam(error, table, trapReceiver, globalProps.ram);
-  session.tableColumns(disks_oid, disks_columns, 20, checkRam);
+      checkInterval = setInterval(() => {
+        
 
-  var cpu_oid = "1.3.6.1.2.1.25.3.3";
-  var cpu_columns = [2];
+        const session = snmp_config.security === 'NoAuthNoPriv'
+          ? snmp.createSession(controlledHostIP, snmp_config.community_name)
+          : snmp.createV3Session (controlledHostIP, getUser())
+      
+        const trapReceiver = snmp.createSession(snmp_config.trap_receiver_ip, snmp_config.community_name);
 
-  const checkCpu = (error, table) => hardware.agentCheckCpu(error, table, trapReceiver, globalProps.cpu);
-  session.tableColumns(cpu_oid, cpu_columns, 20, checkCpu);
+        console.log('deep interval run');
 
-}, 3000);
+        if (snmp_config.inform_network_status) {
+          var network_oid = "1.3.6.1.2.1.2.2";
+          var lan_adapter_columns = [2, 7, 8, 6];
+          const checkAdapters = (error, table) => {
+            global_net_adapters = [...network.agentCheck(error, table, trapReceiver, global_net_adapters)];
+          }
+          session.tableColumns(network_oid, lan_adapter_columns, 1, checkAdapters);
+        }
+      
+        var disks_oid = "1.3.6.1.2.1.25.2.3";
+        var disks_columns = [2, 3, 4, 5, 6];
+      
+        const checkDisks = (error, table) => hardware.agentCheckDisks(error, table, trapReceiver, snmp_config.disk_threshold);
+        session.tableColumns(disks_oid, disks_columns, 20, checkDisks);
+      
+        const checkRam = (error, table) => hardware.agentCheckRam(error, table, trapReceiver, snmp_config.ram_threshold);
+        session.tableColumns(disks_oid, disks_columns, 20, checkRam);
+      
+        var cpu_oid = "1.3.6.1.2.1.25.3.3";
+        var cpu_columns = [2];
+      
+        const checkCpu = (error, table) => hardware.agentCheckCpu(error, table, trapReceiver, snmp_config.cpu_threshold);
+        session.tableColumns(cpu_oid, cpu_columns, 20, checkCpu);
+      }, snmp_config.run_every_in_minute * 60000);
+    }
+  } else console.log('SNMP config was not found');
+}, 20000);
+
+
 
 app.get('/about', function(req, res) {
   res.render('pages/about');
